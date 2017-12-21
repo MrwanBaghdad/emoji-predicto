62d507c30f01d9b10532375050fb7aac1a2b40cd
Amr Fathy <amruafathy@gmail.com>
2017-12-21 18:00:35 +0200

    Use numpy for padding

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,3 +1,3 @@
 from lib.data_cleaning import clean_tweets
-from lib.data_cleaning import process_tweets
 from lib.data_handling import construct_data_matrix
+from lib.data_handling import split_data
--- lib/data_handling.py
+++ lib/data_handling.py
@@ -1,44 +1,43 @@
+import pickle
+
 import fasttext
 import numpy as np
-
-from lib import process_tweets
+from sklearn.model_selection import train_test_split
 
 # noinspection PyUnresolvedReferences
-model = fasttext.load_model('../data/model.bin')
-
-padding_vector = np.array([0 for _ in range(10)])
-
-
-def pad_tweet(tweet, max_word_count):
-    for i in range(len(tweet), max_word_count):
-        tweet.append(padding_vector)
-
-    return tweet
+model = fasttext.load_model('data/model.bin')
 
 
 # noinspection PyShadowingNames
 def construct_data_matrix(tweets, max_word_count):
     data_matrix = []
 
-    for tweet in tweets:
+    for idx, tweet in enumerate(tweets):
         tweet_vector = []
         for word in tweet:
-            word_vector = model[word]
-            tweet_vector.append(word_vector)
+            tweet_vector.append(model[word])
 
-        if len(tweet) < max_word_count:
-            tweet_vector = pad_tweet(tweet_vector, max_word_count)
+        tweet_vector = np.ravel(tweet_vector)
+        padding_len = max_word_count * 100 - len(tweet_vector)  # length of any word vector = 100
+        tweet_vector = np.concatenate((tweet_vector, np.zeros(padding_len)))
 
-        data_matrix.append(
-            np.ravel(tweet_vector)
-        )
+        data_matrix.append(tweet_vector)
 
-    return np.asmatrix(data_matrix)
+    return data_matrix
+
+
+def split_data(data, labels):
+    return train_test_split(data, labels, test_size=0.3)
 
 
 if __name__ == '__main__':
-    tokenized_tweets = process_tweets(open('../data/maro_text').read().splitlines())
+    tokenized_tweets = pickle.load(open('../data/tokenized_tweets.pic', 'rb'))
     tweet_of_max_length = max(tokenized_tweets, key=len)
 
     data_matrix = construct_data_matrix(tokenized_tweets, len(tweet_of_max_length))
-    print(data_matrix.shape)
+    labels = open('../data/maro_labels').read().splitlines()
+
+    data_train, data_test, labels_train, labels_test = split_data(data_matrix, labels)
+
+    print(len(data_train), len(labels_train))
+    print(len(data_test), len(labels_test))
