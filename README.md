5678487d50d024c71a6996bba93c8c9e0ffd4d45
Amr Fathy <amruafathy@gmail.com>
2017-12-21 20:47:54 +0200

    Add Random Forests && Update README

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
+# Multilingual Emoji Prediction
+
 ## Installation
 ```
 sudo pip3 install -r requirements.txt
 
-sudo python3 -m nltk.downloader -d /usr/local/share/nltk_data stopwords
-
-sudo python3 -m nltk.downloader -d /usr/local/share/nltk_data wordnet
-
+sudo python3 -m nltk.downloader -d /usr/local/share/nltk_data stopwords wordnet
 ```
+
+### NER
+To use Named Entity Recognition, additional NLTK datasets need to be installed:
+```
+sudo python3 -m nltk.downloader -d /usr/local/share/nltk_data punkt averaged_perceptron_tagger maxent_ne_chunker
+```
+
+## Run
+- Execute data cleaning script to generate cleaned tweets and labels files.
+- Execute embedding script to generate fasttext model
+- Create your classification model
---
+++ predict_rf.py
@@ -0,0 +1,26 @@
+import pickle
+from os.path import abspath
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+from lib import construct_data_matrix, split_data
+
+tokenized_tweets = pickle.load(open(abspath('data/tokenized_tweets.pic'), 'rb'))
+print('loaded tweets')
+
+data_matrix = construct_data_matrix(tokenized_tweets)
+print('constructed data matrix')
+print('Dim:', data_matrix.shape)
+print('Density:', np.count_nonzero(data_matrix) / np.size(data_matrix))
+
+labels = np.asarray(open(abspath('data/cleaned_labels')).read().splitlines())
+data_train, data_test, labels_train, labels_test = split_data(data_matrix, labels)
+print('split data')
+
+clf = DecisionTreeClassifier(max_depth=5)
+clf.fit(data_train, labels_train)
+print('trained model')
+
+score = clf.score(data_test, labels_test)
+print(score)
